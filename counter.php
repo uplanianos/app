b81856ab2c05a415153232e66bd57a4100fc6d5e
Juan Eduardo M. <134231419+uplanianos@users.noreply.github.com>
2026-03-05 16:55:53 -0300

    Add files via upload

--- counter.php
+++ counter.php
@@ -2,7 +2,9 @@
 session_start();
 
 // Google App Script Web App URL
-$url = "https://script.google.com/macros/s/AKfycbxsJWgBNT1agxWG6jedT1NAsnZFht4UkaevkFh8HuEGX8wFDP7sl7Pp5j6Z8OyH3APe/exec";
+// $url = "https://script.google.com/macros/s/AKfycbxsJWgBNT1agxWG6jedT1NAsnZFht4UkaevkFh8HuEGX8wFDP7sl7Pp5j6Z8OyH3APe/exec";
+$url = "https://script.google.com/macros/library/d/1XscoU5G8rgZ9NsqDJbP_4OQ30QHeLST6rrFiKPDxBdEaMCXxya4OkC65/1";
+
 
 // Check if visitor has already visited in this session
 if(!isset($_SESSION['hasVisited'])){
