a64b0aa1e3880c51d07da4e80731ffec6c4c2f47
Juan Eduardo M. <134231419+uplanianos@users.noreply.github.com>
2026-03-05 17:11:41 -0300

    Add files via upload

--- counter.php
+++ counter.php
@@ -1,21 +1,8 @@
-<?php
-session_start();
+<!DOCTYPE html>
+<html>
 
-// Google App Script Web App URL
-$url = "https://script.google.com/macros/s/AKfycbxsJWgBNT1agxWG6jedT1NAsnZFht4UkaevkFh8HuEGX8wFDP7sl7Pp5j6Z8OyH3APe/exec";
+<body>
+    <p><?php include 'counter-script.php'; ?></p>
+</body>
 
-
-// Check if visitor has already visited in this session
-if(!isset($_SESSION['hasVisited'])){
-    // Increment the counter by visiting the script URL
-    file_get_contents($url);
-    $_SESSION['hasVisited'] = "yes";
-}
-
-// Optional: Display the count (requires reading from sheet,
-// which is more complex; usually, you just record in sheets
-// and display a local text file instead)
-echo "Thank you for visiting!";
-//
-
-?>
+</html>
