e001f7f1635e8945f72ef69c0f5a3c62ac734419
Juan Eduardo M. <134231419+uplanianos@users.noreply.github.com>
2026-08-09 16:09:22 -0400

    Add files via upload

---
+++ scripts/attendDBs.js
@@ -0,0 +1,24 @@
+
+// ROSTERS
+// ==============================
+let rosterAsistencia = "https://docs.google.com/spreadsheets/d/1d2-2EqG3K72DLa7JSroDjxzIrlvWDWDdh_RjK_ILkc0/view?rm=minimal"
+let rosterFacDeArtes = "https://docs.google.com/spreadsheets/d/1Bcd4IAwpEU40wdMkVwXOeAmR6K64zKOwEDboAOS1qwE/view?rm=minimal"
+let rosterFacCiencia = "https://docs.google.com/spreadsheets/d/1ogcY2wp3Nb9PfT90IU3jksSJPc9JZhIcMsAMQnXdaec/view?rm=minimal"
+let rosterFacDeEduca = "https://docs.google.com/spreadsheets/d/1qtEIGS2r9DFL_jkmu61G3Nf6Hsjyq7HiSu0RdvqtGiI/view?rm=minimal"
+let rosterFacEducFis = "https://docs.google.com/spreadsheets/d/1yRZRf8CLySNOUf6cbCQbXy37dBqcRIy1taqc7yc3DO8/view?rm=minimal"
+let rosterFacHumanis = "https://docs.google.com/spreadsheets/d/1MAUVaOnGf6CINtQcxwsX9oRKz_JdN-cdD7mj17Rf9Yc/view?rm=minimal"
+let rosterFacIngenis = "https://docs.google.com/spreadsheets/d/1xgKRt-OH7OPZ8I5ZMns1nmYHKEeF2tRn-EOLSxp_L4I/view?rm=minimal"
+let rosterFacDeSalud = "https://docs.google.com/spreadsheets/d/1iZFpNVDphKlT_93nZdZ6lhT3DSxmLVF_bzSPcE_sIq0/view?rm=minimal"
+let rosterScSociales = "https://docs.google.com/spreadsheets/d/1qtA2IqvZjMeW8BHCAvPyG4zlc8FrdsTnM9jS3UmpTH4/view?rm=minimal"
+
+// FACULTIES [FORM URLs]
+// ==============================
+var urlAsistencia = "https://script.google.com/macros/s/AKfycbw4WvdSGs6FWdGiNK3M6SWECweStPrV3EAh17MNubtORkaOztb-LPBLWfoi_1Bdhphu/exec"
+var urlFacDeArtes = "https://script.google.com/macros/s/AKfycbwTz9JVbcwvobOn5u8ReRFtcyD70Mpkke9b-SjH237VelItV2CXBj7676EPM99hexCo/exec"
+var urlFacCiencia = "https://script.google.com/macros/s/AKfycbxwtGpzj2Oxfgqr1Q3mqBD9THaZzKmTNTK7aZYMQX4dBnYCOkwKp6mr7ClBxLPGszPN/exec"
+var urlFacDeEduca = "https://script.google.com/macros/s/AKfycbw2wlP026Dqs4VxTWnZnmDERV5YinIKb29k4Bp8vADX69HOBlPgLpWNbBPpWW99dxyGLA/exec"
+var urlFacEducFis = "https://script.google.com/macros/s/AKfycbyRBYAiZwcRIpNaxNa233FJ6TE0jAp5b9ADOrcjhvudmuH8XXsZbjlrh63bGcym_0ixuw/exec"
+var urlFacHumanis = "https://script.google.com/macros/s/AKfycbzT9cQiFBU3MdwbTAxfjBTxo1OOU4DJKSiiPc5dFELa8wFMi2XkegJgFAqTQNmMuZoK/exec"
+var urlFacIngenis = "https://script.google.com/macros/s/AKfycbz3RsR9Lh7KB6CRDAHCS_GYXedlDgqJQPDngTv5aau4pZ3mr4W_dErsYzzgFgVkuPS8/exec"
+var urlFacDeSalud = "https://script.google.com/macros/s/AKfycbyaFvALYgdzAbR0rXc_r29vglmPskbsJ5Qw8hHjgO6REdxi5tVTKNmK6t15IZdaAIfnTg/exec"
+var urlScSociales = "https://script.google.com/macros/s/AKfycbwAEAEgF_KxJio_Kr6TWsZWRcKAwfFcSw31fduwpOQGWDwr1DdDm7BxUlK8rT2y6ig/exec"
